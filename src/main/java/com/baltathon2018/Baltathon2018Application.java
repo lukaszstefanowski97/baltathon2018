8ec16160356a33593c7c2ebeff67634c39ba4ee5
RoboBlog <robovlogg@gmail.com>
2018-03-17 14:56:14 +0100

    Initial commit

---
+++ src/main/java/com/baltathon2018/Baltathon2018Application.java
@@ -0,0 +1,12 @@
+package com.baltathon2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baltathon2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baltathon2018Application.class, args);
+	}
+}
